ff9fb382cafff58ec6071080859e6e77e55b3bce
Santhosh Singirikonda <ssingirikonda@netflix.com>
2021-06-19 22:40:03 -0700

    make it fast/clean

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods =['get','post'])
 def index():
-    return "<h1>this is flask application</h1>"
+    return "<h1>this is flask application created by Me</h1>"
 
 if __name__ == "__main__":
     app.run()
